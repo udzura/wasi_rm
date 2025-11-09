d180c963c4b5aa7f6229077c6b1a91447b6e51f5
Uchio Kondo <udzura@udzura.jp>
2025-11-09 21:07:41 +0900

    Suppot RM_WASM_PATH env var for rm.sh

--- rm.sh
+++ rm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-RM_WASM_PATH=target/wasm32-wasip1/release/rm.wasm
+RM_WASM_PATH=${RM_WASM_PATH:-target/wasm32-wasip1/release/rm.wasm}
 wasmtime --dir `pwd` --env PWD=`pwd` $RM_WASM_PATH "$@"
